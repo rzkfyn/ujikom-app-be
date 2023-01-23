62318976b44ff7f79ce09c2f24451a0448ebd848
rzkfyn <rzkfyn@gmail.com>
2023-01-23 16:33:23 +0700

    feat: email verification code model

---
+++ app/models/EmailVerificationCode.ts
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize';
+import Database  from '../core/Database.js';
+
+const EmailVerificationCode = Database.define('EmailVerificationCode', {
+  user_id: {
+    type: DataTypes.BIGINT,
+    allowNull: false
+  },
+  code: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  expired_at: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  deleted_at: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+});
+
+export default EmailVerificationCode;
