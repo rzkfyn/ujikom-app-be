27f13fd3b583a7f6a5c611ab309bde44df0d208a
rzkfyn <rzkfyn@gmail.com>
2023-01-26 19:10:11 +0700

    feat: get user

--- app/controllers/UserController.ts
+++ app/controllers/UserController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express';
 import { nanoid } from 'nanoid';
 import EmailVerificationCode from '../models/EmailVerificationCode.js';
+import Profile from '../models/Profile.js';
+import ProfileMedia from '../models/ProfileMedia.js';
 import User from '../models/User.js';
 import MailService from '../services/MailService.js';
 import type {
+  user as userType,
   emailVerificationCode as emailVerificationCodeType,
-  user as userType
+  profile as profileType,
+  profileMedia as profileMediaType
 } from '../types/types.js';
 
 class UserController {
@@ -61,6 +65,58 @@ class UserController {
     }
 
     return res.status(200).json({ status: 'Ok', message: 'Success, new email verification code has been sent' });
+  };
+
+  public static getUser = async (req: Request, res: Response) => {
+    let { username } = req.params;
+    const { userData } = req.body;
+    if (!username) username = userData.username;
+
+    let user: userType | null;
+    let profile: profileType | null;
+    let profileImage;
+    let coverImage;
+    try {
+      user = await User.findOne({ where: { username } }) as userType | null;
+      if (!user) return res.status(404).json({ status: 'Error', message: 'User not found' });
+      profile = await Profile.findOne({ where: { user_id: user.id } }) as profileType | null;
+      if (!profile) throw new Error();
+      profileImage = await ProfileMedia.findOne({ where: { context: 'PROFILE_IMAGE', profile_id: profile.id } }) as profileMediaType | null;
+      coverImage =  await ProfileMedia.findOne({ where: { context: 'COVER_IMAGE', profile_id: profile.id } }) as profileMediaType | null;
+    } catch(e) {
+      console.log(e);
+      return res.status(500).json({ status: 'Error', message: 'Internal server error' });
+    }
+
+    const { email, username: userName } = user;
+    const { bio } = profile;
+    const isMe = (email === userData.email) && (userName === userData.username) && (user.id === userData.id);
+
+    return res.status(200).json({
+      status: 'Ok',
+      message: 'User fetched successfully',
+      data: {
+        user: {
+          email,
+          userName,
+          name: user.name,
+          created_at: user.createdAt,
+          updated_at: user.updatedAt,
+          profile: {
+            bio,
+            profile_image: {
+              file_name: profileImage?.file_name ?? 'default.jpg',
+              file_mime_type: profileImage?.file_mime_type ?? 'default.jpg',
+            },
+            cover_image: {
+              file_name: coverImage?.file_name,
+              file_mime_type: coverImage?.file_mime_type,
+            }
+          },
+          isMe
+        }
+      }
+    });
   };
 }
 
