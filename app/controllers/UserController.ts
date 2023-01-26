9a79f7c45de5f6c3e1e70956aa44442c149d00d0
rzkfyn <rzkfyn@gmail.com>
2023-01-26 19:19:37 +0700

    feat: update cover image

--- app/controllers/UserController.ts
+++ app/controllers/UserController.ts
@@ -140,13 +140,12 @@ class UserController {
     const { userData } = req.body;
     const profile_image = req.files?.profile_image as UploadedFile | null;
 
-    if (!profile_image) return res.status(400).json({ status: 'Error', message: 'profile image\'s file is required' });
+    if (!profile_image) return res.status(400).json({ status: 'Error', message: 'Profile image\'s file is required' });
     if (profile_image.size > (5 * 1024 * 1024)) return res.status(400).json({ status: 'Error', message: 'Please upload an image lower than 5 mb.' });
 
     try {
       const newFileName = `${+ new Date()}${profile_image.name.split('.')[0]}.${profile_image.name.split('.')[profile_image.name.split('.').length-1]}`;
       const profile = await Profile.findOne({ where: { user_id: userData.id } }) as profileType | null;
-      console.log(newFileName);
       await ProfileMedia.update({ file_mime_type: profile_image.mimetype, file_name: newFileName }, { where: { profile_id: profile?.id, context: 'PROFILE_IMAGE' } });
       await profile_image.mv(`public/media/images/profile_images/${newFileName}`);
     }catch(e) {
@@ -155,6 +154,26 @@ class UserController {
     }
 
     return res.status(200).json({ status: 'Ok', message: 'Profile image changed successfully' });
+  };
+
+  public static updateCoverImage = async (req: Request, res: Response) => {
+    const { userData } = req.body;
+    const cover_image = req.files?.cover_image as UploadedFile;
+
+    if (!cover_image) return res.status(400).json({ status: 'Error', message: 'Cover image\'s file is required' });
+    if (cover_image.size > (5 * 1024 * 1024)) return res.status(400).json({ status: 'Error', message: 'Please upload an image lower than 5 mb.' });
+
+    try {
+      const newFileName = `${+ new Date()}${cover_image.name.split('.')[0]}.${cover_image.name.split('.')[cover_image.name.split('.').length-1]}`;
+      const profile = await Profile.findOne({ where: { user_id: userData.id } }) as profileType | null;
+      await ProfileMedia.update({ file_mime_type: cover_image.mimetype, file_name: newFileName }, { where: { profile_id: profile?.id, context: 'COVER_IMAGE' } });
+      await cover_image.mv(`public/media/images/cover_images/${newFileName}`);
+    } catch(e) {
+      console.log(e);
+      return res.status(500).json({ status: 'Error', message: 'Internal server error' });
+    }
+
+    return res.status(200).json({ status: 'Ok', message: 'Cover image changed successfully' });
   };
 }
 
