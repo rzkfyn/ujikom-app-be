f0b2100e2c6c1e9c783ea0d0028516112ca7ac44
rzkfyn <rzkfyn@gmail.com>
2023-01-31 19:57:49 +0700

    feat: use time out to prevent error

--- database/commands/refresh.ts
+++ database/commands/refresh.ts
@@ -7,16 +7,19 @@ import { resolve } from 'path';
     .map((table) => table.toLowerCase().replace(/(.ts|.js)/, ''));
   try {
     tables.forEach(async (table) => {
+      await Database.query(`DROP TABLE IF EXISTS ${table};`);
       const tableName = table.endsWith('y') ? table.replace('y', 'ies') : `${table}s`;
       await Database.query(`DROP TABLE IF EXISTS ${tableName};`);
     });
 
-    tables = readdirSync(resolve('app', 'models'))
-      .map((table) => table.replace(/(.ts)/, '.js'));
-    tables.forEach(async (table) => {
-      await import(`../../app/models/${table}`);
-      await Database.sync();
-    });
+    setTimeout(async () => {
+      tables = readdirSync(resolve('app', 'models'))
+        .map((table) => table.replace(/(.ts)/, '.js'));
+      tables.forEach(async (table) => {
+        await import(`../../app/models/${table}`);
+        await Database.sync();
+      });
+    }, 3000);
   } catch(e) {
     console.log(e);
   }
