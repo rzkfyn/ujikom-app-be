2fa47827f54e3a31291a4192f700bac243983a5e
rzkfyn <rzkfyn@gmail.com>
2023-02-03 15:34:09 +0700

    feat: get user connection info & get user following info

--- app/controllers/FollowerController.ts
+++ app/controllers/FollowerController.ts
@@ -51,35 +51,13 @@ class FollowerController {
   };
 
   public static getUserFollowersInfo = async (req: Request, res: Response) => {
-    const result = [];
     const { username } = req.params;
 
+    let userFollowers;
     try {
       const user = await User.findOne({ where: { username } }) as Model<userType, userType> | null;
-      if (!user) return res.status(404).json({ status: 'Error', message: 'Internal server error' });
-      const userFollowers = await HasFollower.findAll({ where: { followed_user_id: user?.dataValues.id } }) as unknown;
-  
-      for (const userFollower of userFollowers as Model<hasFollowerType, hasFollowerType>[]) {
-        const user = await User.findOne({ where: { id: userFollower.dataValues.follower_user_id } }) as Model<userType, userType> | null;
-        const profile = await Profile.findOne({ where: { user_id: user?.dataValues.id } }) as Model<profileType, profileType> | null;
-        const profileImage = await ProfileMedia.findOne({ where: { profile_id: profile?.dataValues.id, context: 'PROFILE_IMAGE' } }) as Model<profileMediaType, profileMediaType> | null;
-        const coverImage = await ProfileMedia.findOne({ where: { profile_id: profile?.dataValues.id, context: 'COVER_IMAGE' } }) as Model<profileMediaType, profileMediaType> | null;
-  
-        result.push({
-          name: user?.dataValues.name,
-          username: user?.dataValues.username,
-          profile: {
-            profile_image: {
-              file_name: profileImage?.dataValues.file_name ?? 'default.png',
-              file_mime_type: profileImage?.dataValues.file_mime_type ?? 'image/png'
-            },
-            cover_image: {
-              file_name: coverImage?.dataValues.file_name ?? 'default.png',
-              file_mime_type: coverImage?.dataValues.file_mime_type ?? 'image/png'
-            }
-          }
-        });
-      }
+      if (!user) return res.status(404).json({ status: 'Error', message: 'User not found' });
+      userFollowers = await this.getUserConnectionsInfo(user.dataValues.id);
     } catch(e) {
       console.log(e);
       return res.status(500).json({ status: 'Error', message: 'Internal server error' });
@@ -88,13 +66,61 @@ class FollowerController {
     return res.status(200).json({ 
       status: 'Ok',
       message: 'Successfully fetched user\'s followers info',
-      data: result
+      data: userFollowers
     });
   };
 
-  // private static getUserFollowers = async (userId: number) => {
+  public static getUserFollowingInfo = async (req: Request, res: Response) => {
+    const { username } = req.params;
     
-  // };
+    let userFollowing;
+    try {
+      const user = await User.findOne({ where: { username } }) as Model<userType, userType> | null;
+      if (!user) return res.status(404).json({ status: 'Error', message: 'User not found' });
+      userFollowing = await this.getUserConnectionsInfo(user.dataValues.id, 'following');
+    } catch(e) {
+      console.log(e);
+      return res.status(500).json({ status: 'Error', message: 'Internal server error' });
+    }
+
+    return res.status(200).json({
+      status: 'Ok',
+      message: 'Successfully fetched user\'s following info',
+      data: userFollowing
+    });
+  };
+
+  private static getUserConnectionsInfo = async (userId: number, context = 'follower') => {
+    const result = [];
+    if (!userId)  throw new Error();
+    const where = context === 'follower' ? { followed_user_id: userId } : { follower_user_id: userId };
+    const userConnections = await HasFollower.findAll({ where }) as unknown;
+
+    for (const userFollower of userConnections as Model<hasFollowerType, hasFollowerType>[]) {
+      const fetchUserWhere = context === 'follower' ? { id: userFollower.dataValues.follower_user_id } : { id: userFollower.dataValues.followed_user_id };
+      const user = await User.findOne({ where: fetchUserWhere }) as Model<userType, userType> | null;
+      const profile = await Profile.findOne({ where: { user_id: user?.dataValues.id } }) as Model<profileType, profileType> | null;
+      const profileImage = await ProfileMedia.findOne({ where: { profile_id: profile?.dataValues.id, context: 'PROFILE_IMAGE' } }) as Model<profileMediaType, profileMediaType> | null;
+      const coverImage = await ProfileMedia.findOne({ where: { profile_id: profile?.dataValues.id, context: 'COVER_IMAGE' } }) as Model<profileMediaType, profileMediaType> | null;
+
+      result.push({
+        name: user?.dataValues.name,
+        username: user?.dataValues.username,
+        profile: {
+          profile_image: {
+            file_name: profileImage?.dataValues.file_name ?? 'default.png',
+            file_mime_type: profileImage?.dataValues.file_mime_type ?? 'image/png'
+          },
+          cover_image: {
+            file_name: coverImage?.dataValues.file_name ?? 'default.png',
+            file_mime_type: coverImage?.dataValues.file_mime_type ?? 'image/png'
+          }
+        }
+      });
+    }
+
+    return result;
+  };
 }
 
 export default FollowerController;
