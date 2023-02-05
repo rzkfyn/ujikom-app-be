697d76ad7801467f601411f1b6eac9f5b1f86f85
rzkfyn <rzkfyn@gmail.com>
2023-02-05 20:41:51 +0700

    feat: remove follower & some adjusment

--- app/controllers/FollowerController.ts
+++ app/controllers/FollowerController.ts
@@ -1,17 +1,16 @@
 import { Request, Response } from 'express';
 import { Model } from 'sequelize';
 import User from '../models/User.js';
-import Profile from '../models/Profile.js';
 import HasFollower from '../models/HasFollower.js';
+import UserService from '../services/UserService.js';
 import type {
   user as userType,
   hasFollower as hasFollowerType,
-  profile as profileType,
-  profileMedia as profileMediaType
 } from '../types/types.js';
-import ProfileMedia from '../models/ProfileMedia.js';
 
 class FollowerController {
+  private static userService = new UserService();
+
   public static follow = async (req: Request, res: Response) => {
     const { username } = req.params;
     const { userData } = req.body;
@@ -55,9 +54,7 @@ class FollowerController {
 
     let userFollowers;
     try {
-      const user = await User.findOne({ where: { username } }) as Model<userType, userType> | null;
-      if (!user) return res.status(404).json({ status: 'Error', message: 'User not found' });
-      userFollowers = await this.getUserConnectionsInfo(user.dataValues.id);
+      userFollowers = await this.userService.getUserConnectionsInfo(username);
     } catch(e) {
       console.log(e);
       return res.status(500).json({ status: 'Error', message: 'Internal server error' });
@@ -75,9 +72,7 @@ class FollowerController {
     
     let userFollowing;
     try {
-      const user = await User.findOne({ where: { username } }) as Model<userType, userType> | null;
-      if (!user) return res.status(404).json({ status: 'Error', message: 'User not found' });
-      userFollowing = await this.getUserConnectionsInfo(user.dataValues.id, 'following');
+      userFollowing = await this.userService.getUserConnectionsInfo(username, 'following');
     } catch(e) {
       console.log(e);
       return res.status(500).json({ status: 'Error', message: 'Internal server error' });
@@ -90,36 +85,22 @@ class FollowerController {
     });
   };
 
-  private static getUserConnectionsInfo = async (userId: number, context = 'follower') => {
-    const result = [];
-    if (!userId)  throw new Error();
-    const where = context === 'follower' ? { followed_user_id: userId } : { follower_user_id: userId };
-    const userConnections = await HasFollower.findAll({ where }) as unknown;
+  public static removeFollower = async (req: Request, res: Response) => {
+    const { username } = req.params;
+    const { userData } = req.body;
 
-    for (const userFollower of userConnections as Model<hasFollowerType, hasFollowerType>[]) {
-      const fetchUserWhere = context === 'follower' ? { id: userFollower.dataValues.follower_user_id } : { id: userFollower.dataValues.followed_user_id };
-      const user = await User.findOne({ where: fetchUserWhere }) as Model<userType, userType> | null;
-      const profile = await Profile.findOne({ where: { user_id: user?.dataValues.id } }) as Model<profileType, profileType> | null;
-      const profileImage = await ProfileMedia.findOne({ where: { profile_id: profile?.dataValues.id, context: 'PROFILE_IMAGE' } }) as Model<profileMediaType, profileMediaType> | null;
-      const coverImage = await ProfileMedia.findOne({ where: { profile_id: profile?.dataValues.id, context: 'COVER_IMAGE' } }) as Model<profileMediaType, profileMediaType> | null;
-
-      result.push({
-        name: user?.dataValues.name,
-        username: user?.dataValues.username,
-        profile: {
-          profile_image: {
-            file_name: profileImage?.dataValues.file_name ?? 'default.png',
-            file_mime_type: profileImage?.dataValues.file_mime_type ?? 'image/png'
-          },
-          cover_image: {
-            file_name: coverImage?.dataValues.file_name ?? 'default.png',
-            file_mime_type: coverImage?.dataValues.file_mime_type ?? 'image/png'
-          }
-        }
-      });
+    try {
+      const user = await User.findOne({ where: { username } }) as Model<userType, userType>;
+      if (!user) return res.status(404).json({ status: 'Error', message: 'User not found' });
+      const hasFollower = await HasFollower.findOne({ where: { followed_user_id: userData.id, follower_user_id: user.dataValues.id } }) as Model<hasFollowerType, hasFollowerType> | null;
+      if (!hasFollower) return res.status(400).json({ status: 'Error', message: `${username} didn't follow your account` });
+      await hasFollower.destroy();
+    } catch(e) {
+      console.log(e);
+      return res.status(500).json({ status: 'Error', message: 'Internal server error' });
     }
 
-    return result;
+    return res.status(200).json({ status: 'Ok', message: `${username} removed from your follower list` });
   };
 }
 
