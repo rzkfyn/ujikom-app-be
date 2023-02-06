babe8740b919c419b74959486f1de88a5b4470c4
rzkfyn <rzkfyn@gmail.com>
2023-02-06 20:56:47 +0700

    feat: call association on startup

--- index.ts
+++ index.ts
@@ -5,17 +5,19 @@ import cors from 'cors';
 import fileUpload from 'express-fileupload';
 import 'dotenv/config';
 import Database from './app/core/Database.js';
+import { associationsInit } from './database/commands/associations.js';
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 try {
   await Database.authenticate();
+  associationsInit();
 } catch(e) {
   console.log(e);
 }
 
-app.use(cors({ origin: ['http://127.0.0.1:5173', 'http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://127.0.0.1:5173', 'http://localhost:5173' ], credentials: true }));
 app.use(json());
 app.use(expressStatic('public'));
 app.use(urlencoded({ extended: false }));
