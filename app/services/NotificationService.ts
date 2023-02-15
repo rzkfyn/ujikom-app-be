114860cf6592730b765432f333cb4ff75e1dac4c
rzkfyn <rzkfyn@gmail.com>
2023-02-15 21:17:33 +0700

    feat(notif-service): remove notification

--- app/services/NotificationService.ts
+++ app/services/NotificationService.ts
@@ -12,6 +12,17 @@ class NotificationService {
     });
     return notification?.toJSON();
   };
+
+  public removeNotification = async (receiverUserId: number, firedByUserId: number, context: event, transaction: Transaction) => {
+    const notification = await Notification.findOne({ 
+      where: {
+        receiver_user_id: receiverUserId, fired_by_user_id: firedByUserId, event: context
+      }
+    });
+    if (!notification) return false;
+    await notification.destroy({ transaction });
+    return true;
+  };
 }
 
 export default NotificationService;
