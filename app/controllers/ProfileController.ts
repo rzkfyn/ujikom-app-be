22053fdeacd0060fba70c9b7e437d5c32a09c9ac
rzkfyn <rzkfyn@gmail.com>
2023-02-18 19:41:22 +0700

    feat: update profile adjusment & refactor
    moved all function related profile media to it's
    own file

--- app/controllers/ProfileController.ts
+++ app/controllers/ProfileController.ts
@@ -1,69 +1,29 @@
 import { Request, Response } from 'express';
-import { Model } from 'sequelize';
-import { UploadedFile } from 'express-fileupload';
 import Profile from '../models/Profile.js';
 import User from '../models/User.js';
-import ProfileMedia from '../models/ProfileMedia.js';
-import type { profile as profileType } from '../types/types.js';
+import Database from '../core/Database.js';
 
 class ProfileController {
   public static updateProfile = async (req: Request, res: Response) => {
-    const { bio, location, name } = req.body;
+    const { bio, location, name, url, date_of_birth } = req.body;
     const { auth } = req.body;
     const { user: authorizedUser } = auth;
+    if (!date_of_birth) return res.status(400).json({ status: 'Error', message: 'Date of birth is required!' });
 
+    const transaction = await Database.transaction();
     try {
-      await Profile.update({ location: location ?? null, bio: bio ?? null }, { where: { user_id: authorizedUser.id } });
-      await User.update({ name: name ?? null }, { where: { id: authorizedUser.id} });
+      await Profile.update({ 
+        location: location ?? null, bio: bio ?? null, url: url ?? null, date_of_birth: new Date(date_of_birth).toISOString() 
+      }, { where: { user_id: authorizedUser.id }, transaction });
+      await User.update({ name: name ?? null }, { where: { id: authorizedUser.id}, transaction });
+      await transaction.commit();
     } catch(e) {
       console.log(e);
+      await transaction.rollback();
       return res.status(500).json({ status: 'Error', message: 'Internal server error' });
     }
 
     return res.status(200).json({ status: 'Ok', message: 'Profile updated successfully' });
-  };
-
-  public static updateProfileImage = async (req: Request, res: Response) => {
-    const { auth } = req.body;
-    const { user: authorizedUser } = auth;
-    const profile_image = req.files?.profile_image as UploadedFile | null;
-
-    if (!profile_image) return res.status(400).json({ status: 'Error', message: 'Profile image\'s file is required' });
-    if (profile_image.size > (5 * 1024 * 1024)) return res.status(400).json({ status: 'Error', message: 'Please upload an image lower than 5 mb.' });
-
-    try {
-      const newFileName = `${+ new Date()}${profile_image.name.split('.')[0]}.${profile_image.name.split('.')[profile_image.name.split('.').length-1]}`;
-      const profile = await Profile.findOne({ where: { user_id: authorizedUser.id } }) as Model<profileType, profileType>;
-      console.log(profile);
-      await ProfileMedia.update({ file_mime_type: profile_image.mimetype, file_name: newFileName }, { where: { profile_id: profile.dataValues.id, context: 'PROFILE_IMAGE' } });
-      await profile_image.mv(`public/media/images/profile_images/${newFileName}`);
-    }catch(e) {
-      console.log(e);
-      return res.status(500).json({ status: 'Error', message: 'Internal server error' });
-    }
-
-    return res.status(200).json({ status: 'Ok', message: 'Profile image changed successfully' });
-  };
-
-  public static updateCoverImage = async (req: Request, res: Response) => {
-    const { auth } = req.body;
-    const { user: authorizedUser } = auth;
-    const cover_image = req.files?.cover_image as UploadedFile;
-
-    if (!cover_image) return res.status(400).json({ status: 'Error', message: 'Cover image\'s file is required' });
-    if (cover_image.size > (5 * 1024 * 1024)) return res.status(400).json({ status: 'Error', message: 'Please upload an image lower than 5 mb.' });
-
-    try {
-      const newFileName = `${+ new Date()}${cover_image.name.split('.')[0]}.${cover_image.name.split('.')[cover_image.name.split('.').length-1]}`;
-      const profile = await Profile.findOne({ where: { user_id: authorizedUser.id } }) as Model<profileType, profileType>;
-      await ProfileMedia.update({ file_mime_type: cover_image.mimetype, file_name: newFileName }, { where: { profile_id: profile.dataValues.id, context: 'COVER_IMAGE' } });
-      await cover_image.mv(`public/media/images/cover_images/${newFileName}`);
-    } catch(e) {
-      console.log(e);
-      return res.status(500).json({ status: 'Error', message: 'Internal server error' });
-    }
-
-    return res.status(200).json({ status: 'Ok', message: 'Cover image changed successfully' });
   };
 }
 
