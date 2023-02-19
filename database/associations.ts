db8b0e303bc63451a06904c68ebf6fe1fc58a8ba
rzkfyn <rzkfyn@gmail.com>
2023-02-19 21:24:46 +0700

    feat(association): various changes & move file

---
+++ database/associations.ts
@@ -0,0 +1,77 @@
+import User from '../app/models/User.js';
+import Profile from '../app/models/Profile.js';
+import ProfileMedia from '../app/models/ProfileMedia.js';
+import Post from '../app/models/Post.js';
+import HasFollower from '../app/models/HasFollower.js';
+import HasBlocker from '../app/models/HasBlocker.js';
+import PostMedia from '../app/models/PostMedia.js';
+import PostLike from '../app/models/PostLike.js';
+import Notification from '../app/models/Notification.js';
+import MentionedUserOnPost from '../app/models/MentionedUserOnPost.js';
+import SavedPost from '../app/models/SavedPost.js';
+
+export const associationsInit = () => {
+  User.belongsToMany(User, { 
+    through: {
+      model: HasFollower,
+      paranoid: true
+    }, foreignKey: 'followed_user_id', as: 'followers' });
+  User.belongsToMany(User, { 
+    through: {
+      model: HasFollower,
+      paranoid: true
+    }, foreignKey: 'follower_user_id', as: 'following' });
+  User.belongsToMany(User, { 
+    through: {
+      model: HasBlocker,
+      paranoid: true
+    }, foreignKey: 'blocked_user_id', as: 'blocker' });
+  User.belongsToMany(User, {
+    through:  {
+      model: HasBlocker,
+      paranoid: true
+    }, foreignKey: 'blocker_user_id', as: 'blocking' });
+  User.hasOne(Profile, { foreignKey: 'user_id', as: 'profile' });
+  Profile.belongsTo(User, { foreignKey: 'user_id', as: 'profile' });
+  Profile.hasMany(ProfileMedia, { foreignKey: 'profile_id', as: 'profile_media' });
+  ProfileMedia.belongsTo(Profile, { foreignKey: 'profile_id', as: 'profile' });
+  User.hasMany(Post, { foreignKey: 'user_id', as: 'posts' });
+  Post.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+  Post.hasMany(PostMedia, { foreignKey: 'post_id', as: 'media' });
+  PostMedia.belongsTo(Post, { foreignKey: 'post_id', as: 'post' });
+  Post.belongsToMany(User, {
+    through: {
+      model: PostLike,
+      paranoid: true,
+    }, foreignKey: 'post_id', as: 'likers' });
+  User.belongsToMany(Post, {
+    through: {
+      model: PostLike,
+      paranoid: true
+    }, foreignKey: 'user_id', as: 'liked_posts' });
+  Notification.belongsTo(User, { foreignKey: 'fired_by_user_id', as: 'fired_by_user' });
+  Post.belongsToMany(User, {
+    through: {
+      model: MentionedUserOnPost,
+      paranoid: true
+    }, foreignKey: 'post_id', as: 'mentioned_users'});
+  User.belongsToMany(Post, {
+    through: {
+      model: MentionedUserOnPost,
+      paranoid: true
+    }, foreignKey: 'user_id', as: 'mentioned_on_posts'});
+  Post.belongsToMany(User, {
+    through: {
+      model: SavedPost,
+      paranoid: true
+    }, foreignKey: 'post_id', as: 'saved_by_users' });
+  User.belongsToMany(Post, {
+    through: {
+      model: SavedPost,
+      paranoid: true
+    }, foreignKey: 'user_id', as: 'saved_posts' });
+  // User.hasMany(SavedPost, { foreignKey: 'user_id', as: 'saved_post' });
+  // SavedPost.belongsTo(User, { foreignKey: 'user_id', as: 'saved_by_user' });
+  Post.hasMany(SavedPost, { foreignKey: 'post_id', as: 'saved_post_vipot' });
+  SavedPost.belongsTo(Post, { foreignKey: 'post_id', as: 'post' });
+};
