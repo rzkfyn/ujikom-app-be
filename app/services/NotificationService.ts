7777026937d56ebb1ce67178781f617d4dd7ad1f
rzkfyn <rzkfyn@gmail.com>
2023-02-27 16:23:39 +0700

    feat: remove notifications by event

--- app/services/NotificationService.ts
+++ app/services/NotificationService.ts
@@ -1,29 +1,38 @@
 import { Transaction } from 'sequelize';
 import Notification from '../models/Notification.js';
+import eventEmitter from '../core/Event.js';
+import Comment from '../models/Comment.js';
 
 type event = 'USER_FOLLOW' | 'USER_MENTION_ON_POST' | 'USER_MENTION_ON_BIO' | 'USER_MENTION_ON_COMMENT' | 'POST_LIKE' | 'POST_SHARE' | 'POST_COMMENT' | 'COMMENT_LIKE' | 'COMMENT_REPLY';
 
 class NotificationService {
-  public createNotification = async (receiverUserId: number, firedByUserId: number, context: event, transaction: Transaction) => {
+  public createNotification = async (receiverUserId: number, firedByUserId: number, context: event, relatedEventId: number | null, transaction: Transaction) => {
     if (receiverUserId === firedByUserId) return true;
 
     const notification = await Notification.create({ 
-      receiver_user_id: receiverUserId, fired_by_user_id: firedByUserId, event: context 
+      receiver_user_id: receiverUserId, fired_by_user_id: firedByUserId, event: context, related_event_id: relatedEventId
     }, {
       transaction 
     });
+    eventEmitter.emit('notificationchange', receiverUserId);
     return notification?.toJSON();
   };
 
-  public removeNotification = async (receiverUserId: number, firedByUserId: number, context: event, transaction: Transaction) => {
+  public removeNotification = async (receiverUserId: number, firedByUserId: number, context: event, relatedEventId: number | null, transaction: Transaction) => {
     const notification = await Notification.findOne({ 
       where: {
-        receiver_user_id: receiverUserId, fired_by_user_id: firedByUserId, event: context
+        receiver_user_id: receiverUserId, fired_by_user_id: firedByUserId, event: context, related_event_id: relatedEventId
       }
     });
     if (!notification) return false;
     await notification.destroy({ transaction });
+    eventEmitter.emit('notificationchange', receiverUserId);
     return true;
+  };
+
+  public removeNotificationsByEvent = async (context: string, relatedEventId: number, transaction: Transaction) => {
+    if (context === 'post') return await Notification.destroy({ where: { related_event_id: relatedEventId }, transaction });
+    if (context === 'comment') return await Comment.destroy({ where: { related_event_id: relatedEventId }, transaction });
   };
 }
 
