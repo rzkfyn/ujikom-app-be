a5d0709a4c7b0a28a5d03e0a78be0b8c80c2bdb3
rzkfyn <rzkfyn@gmail.com>
2023-02-28 17:29:35 +0700

    feat(event): update user presence on user connect and disconnect events

--- app/events/userConnect.ts
+++ app/events/userConnect.ts
@@ -1,5 +1,8 @@
+import { Model } from 'sequelize';
 import { Socket } from 'socket.io';
+import Database from '../core/Database.js';
 import eventEmitter from '../core/Event.js';
+import UserPresence from '../models/UserPresence.js';
 
 export default {
   name: 'userconnect',
@@ -8,5 +11,25 @@ export default {
   }) => {
     eventEmitter.on('notificationchange', (forUserId: number) => socket.emit('notificationchange', { forUserId }));
     eventEmitter.on('poststatechange', (postCode: string) => socket.emit('poststatechange', { postCode }));
+    if (data === 'guest') return;
+
+    const transaction = await Database.transaction();
+    try {
+      const userPresence = await UserPresence.findOne({ where: { user_id: data } }) as Model | null;
+      if (!userPresence) {
+        await UserPresence.create({ user_id: data, status: 'ONLINE', transaction });
+      } else {
+        await userPresence.update({ status: 'ONLINE', last_seen: null, transaction });
+      }
+      await transaction.commit();
+    } catch(e) {
+      await transaction.rollback();
+    }
+    eventEmitter.emit('userpresencechange', data);
+
+    socket.on('disconnect', async () => {
+      eventEmitter.emit('userpresencechange', data);
+      await UserPresence.update({ status: 'OFFLINE', last_seen: new Date().toISOString() }, { where: { user_id: data } });
+    });
   }
 };
